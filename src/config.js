7fae630073de0c51ead9a31a021a372bda2bf135
CGbrando98 <brandon.shew@capgemini.com>
2022-10-20 14:59:39 -0400

    Set config vars

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const baseUrl =
-  process.env.NODE_ENV === 'development'
-    ? 'https://techn-application-backend.herokuapp.com'
-    : 'http://localhost:8080'
+  process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_MY_PROD_URL
+    : process.env.REACT_APP_MY_DEV_URL
 
 export default baseUrl
